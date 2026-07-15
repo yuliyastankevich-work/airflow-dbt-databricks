a6d8270364fd10ce1d9f8e7b6b5209c8641adde9
yuliyastankevich-work <yuliya.stankevich@innowise.com>
2026-07-15 09:29:23 +0000

    created ephemeral views for gold

---
+++ wallmart_dbt/models/gold/ephemeral/eph_orders.sql
@@ -0,0 +1,15 @@
+SELECT
+DISTINCT
+order_id,
+order_item_id,
+payment_method,
+order_status,
+order_timestamp,
+order_created_timestamp,
+order_updated_timestamp,
+order_is_active,
+order_processed_at,
+obt_b_processed_at,
+current_timestamp() as order_gold_processed_at
+FROM
+{{ ref('obt_b')}}
---
+++ wallmart_dbt/models/gold/ephemeral/eph_stores.sql
@@ -0,0 +1,14 @@
+SELECT
+DISTINCT
+store_id,
+store_name,
+store_city,
+store_province,
+store_country,
+store_created_timestamp,
+store_updated_timestamp,
+store_is_active,
+store_processed_at,
+current_timestamp() as store_gold_processed_at
+FROM
+{{ ref('obt_b')}}
