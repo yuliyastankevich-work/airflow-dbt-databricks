25b9432cb0ad152a0bd34ff3f1c011640adac3b6
yuliyastankevich-work <yuliya.stankevich@innowise.com>
2026-07-16 07:29:45 +0000

    finished project

--- airflow/dags/orchestrate.py
+++ airflow/dags/orchestrate.py
@@ -1,11 +1,33 @@
 from airflow.sdk import dag, task
 from airflow.operators.bash import BashOperator
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.jobs import RunLifeCycleState, RunResultState
+import os
+import time
+
 @dag
 def orchestrate():
 
     @task
     def ingest_cdc():
-        return "CDC data ingested"
+        ws = WorkspaceClient(
+                    host = 'dbc-c44b4dd7-5f31.cloud.databricks.com',
+                    token = os.getenv("DATBRICKS_API_KEY"))
+        job_trigger = ws.jobs.run_now(job_id=988589484506012)
+
+        while True:
+            job_run = ws.jobs.get_run(job_trigger.run_id)
+
+            print(f"Job run status: {job_run.state.life_cycle_state}\nJob result state: {job_run.state.result_state}")
+            if job_run.state.life_cycle_state in [RunLifeCycleState.TERMINATED, RunLifeCycleState.SKIPPED, RunLifeCycleState.INTERNAL_ERROR]:
+                if job_run.state.result_state == RunResultState.SUCCESS:
+                    print("Job completed successfully")
+                    break
+                else:
+                    raise Exception(f"Job failed with state: {job_run.state.result_state}")
+            time.sleep(5)
+        return "CDC ingestion completed"
     
     @task.bash
     def clean_target_logs():
--- airflow/dags/test_conn.py
+++ airflow/dags/test_conn.py
@@ -1,5 +1,8 @@
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.jobs import RunLifeCycleState, RunResultState
+
 import os
+import time
 ws = WorkspaceClient(
     host = 'dbc-c44b4dd7-5f31.cloud.databricks.com',
     token = os.getenv("DATBRICKS_API_KEY")
@@ -8,4 +11,11 @@ job_trigger = ws.jobs.run_now(job_id=988589484506012)
 
 while True:
     job_run = ws.jobs.get_run(job_trigger.run_id)
-    if job_run.state.life_cycle_state in ["TERMINATED", "SKIPPED", "INTERNAL_ERROR"]: pass
+    print(f"Job run status: {job_run.state.life_cycle_state}\nJob result state: {job_run.state.result_state}")
+    if job_run.state.life_cycle_state in [RunLifeCycleState.TERMINATED, RunLifeCycleState.SKIPPED, RunLifeCycleState.INTERNAL_ERROR]:
+        if job_run.state.result_state == RunResultState.SUCCESS:
+            print("Job completed successfully")
+            break
+        else:
+            raise Exception(f"Job failed with state: {job_run.state.result_state}")
+    time.sleep(5)
